086519c8c2271cac3fb4d838bf46934c1ea4b2b0
fekoneko <55813967+fekoneko@users.noreply.github.com>
2024-10-10 02:36:20 +0300

    update it once more

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,9 +1,11 @@
 import { FC } from 'react';
 
 const Footer: FC = () => {
+  const currentYear = new Date().getFullYear();
+
   return (
     <footer role="contentinfo" className="bg-slate-500 px-[10%] py-4 text-white/60 shadow-lg">
-      <p>© 2023 - {new Date().getFullYear()} JapaneseLearn DB.</p>
+      <p>© 2024{currentYear > 2024 && ' - ' + currentYear} JapaneseLearn DB.</p>
       <p>Разработчик сайта: Авдеев Андрей, ИАТЭ НИЯУ МИФИ, группа ИВТ-Б21</p>
     </footer>
   );
