3239527e701e726138ea14ae9826d1b1c8e5836e
fekoneko <55813967+fekoneko@users.noreply.github.com>
2024-10-10 02:32:08 +0300

    update ""copyright"" in footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,7 +3,7 @@ import { FC } from 'react';
 const Footer: FC = () => {
   return (
     <footer role="contentinfo" className="bg-slate-500 px-[10%] py-4 text-white/60 shadow-lg">
-      <p>© 2023 JapaneseLearn DB.</p>
+      <p>© 2023 - {new Date().getFullYear()} JapaneseLearn DB.</p>
       <p>Разработчик сайта: Авдеев Андрей, ИАТЭ НИЯУ МИФИ, группа ИВТ-Б21</p>
     </footer>
   );
