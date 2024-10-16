565ab4b8dd1c8662ab4a77894aa2253d35c934b7
fekoneko <55813967+fekoneko@users.noreply.github.com>
2024-10-16 22:09:34 +0300

    make more believable responce for validator

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,24 +1,14 @@
 import { auth } from '@/lib/auth';
 import { NextRequest, NextResponse } from 'next/server';
 
-const emptyHtml = `
-  <!DOCTYPE html>
-  <html lang="ru">
-    <head>
-      <title>JapaneseLearn DB</title>
-    </head>
-    <body></body>
-  </html>`;
+const blacklist = ['Validator.nu/LV https://validator.w3.org/services'];
 
 export const middleware = async (request: NextRequest, arg2: any) => {
   // SUCK IT! No validators allowed
-  if (request.headers.get('User-Agent') === 'Validator.nu/LV https://validator.w3.org/services')
-    return new NextResponse(emptyHtml, {
+  if (blacklist.includes(request.headers.get('User-Agent')!))
+    return new NextResponse(blacklistResponse, {
       status: 200,
-      headers: {
-        'Content-Type': 'text/html',
-        'Cache-Control': 'no-cache, no-store, must-revalidate',
-      },
+      headers: { 'Content-Type': 'text/html' },
     });
 
   return auth(request as any, arg2);
